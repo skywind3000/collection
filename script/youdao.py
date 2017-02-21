51d080ec3880c5da83f635e562ca344d9b078328
skywind3000 <skywind3000@163.com>
2017-02-21 14:38:24 +0800

    new filter

--- script/youdao.py
+++ script/youdao.py
@@ -792,13 +792,10 @@ class WordCount (object):
 	def reset (self):
 		self._words = {}
 
-	def read (self, fp, filter = None, lower = True, lemma = False):
+	def read (self, fp, filter = None, lower = True):
 		if type(fp) in (type(''), type(u'')):
 			fp = open(fp, 'r')
 		count = 0
-		if lemma:
-			import pattern
-			import pattern.en
 		for line in fp:
 			line = line.strip()
 			if not line:
@@ -820,15 +817,6 @@ class WordCount (object):
 					continue
 				if lower and (not word.islower()):
 					continue
-				if lemma:
-					word = pattern.en.lemma(word)
-					if not word:
-						continue
-					word = word.strip()
-					if not word:
-						continue
-					if not word.isalpha():
-						continue
 				self._words[word] = self._words.get(word, 0) + 1
 				count += 1
 		return count
