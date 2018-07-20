801cd1ff78d6aa5ce00d032ae4bc3241dc10f380
skywind3000 <skywind3000@163.com>
2018-07-21 00:19:47 +0800

    new ascmini.py

--- script/ascmini.py
+++ script/ascmini.py
@@ -8,6 +8,7 @@
 # Last change: 2017/03/24 19:42:40
 #
 #======================================================================
+from __future__ import print_function
 import sys
 import time
 import os
@@ -1008,6 +1009,7 @@ class TraceOut (object):
 		self._encoding = 'utf-8'
 		self._stdout = True
 		self._stderr = False
+		self._makedir = False
 
 	def _writelog (self, *args):
 		now = time.strftime('%Y-%m-%d %H:%M:%S')
@@ -1024,6 +1026,11 @@ class TraceOut (object):
 		if self._logfile is None:
 			import codecs
 			logname = '%s%s.log'%(self._prefix, date)
+			dirname = os.path.dirname(logname)
+			if self._makedir:
+				if not os.path.exists(dirname):
+					try: os.makedirs(dirname)
+					except: pass
 			self._logfile = codecs.open(logname, 'a', self._encoding)
 		part = []
 		for text in args:
