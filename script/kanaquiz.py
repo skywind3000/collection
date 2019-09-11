b0a728ec759849384083effdf3e47464c7247c7b
skywind3000 <skywind3000@163.com>
2019-09-11 19:29:44 +0800

    new kanaquiz.py

--- script/kanaquiz.py
+++ script/kanaquiz.py
@@ -10,6 +10,9 @@
 #======================================================================
 import sys
 import time
+import os
+import codecs
+import json
 
 
 #----------------------------------------------------------------------
@@ -86,5 +89,141 @@ for item in KANAS:
     romaji = item[2]
     ROMAJI[hiragana] = romaji
     ROMAJI[katakana] = romaji
+
+
+#----------------------------------------------------------------------
+# config
+#----------------------------------------------------------------------
+class configure (object):
+
+    def __init__ (self):
+        self.dirhome = os.path.expanduser('~/.cache/kanaquiz')
+        if not os.path.exists(self.dirhome):
+            self.mkdir(self.dirhome)
+        self.config = {}
+        self.limit = 10
+        self.cfgname = os.path.join(self.dirhome, 'quiz.tbl')
+        self.load()
+
+    def mkdir (self, path):
+        unix = sys.platform[:3] != 'win' and True or False
+        path = os.path.abspath(path)
+        if os.path.exists(path):
+            return False
+        name = ''
+        part = os.path.abspath(path).replace('\\', '/').split('/')
+        if unix:
+            name = '/'
+        if (not unix) and (path[1:2] == ':'):
+            part[0] += '/'
+        for n in part:
+            name = os.path.abspath(os.path.join(name, n))
+            if not os.path.exists(name):
+                os.mkdir(name)
+        return True
+
+    def load (self):
+        self.config = {}
+        for item in KANAS:
+            h, k = item[:2]
+            self.config[h] = []
+            self.config[k] = []
+        self.config['h'] = []
+        self.config['k'] = []
+        self.config['a'] = []
+        config = None
+        try:
+            with codecs.open(self.cfgname, 'r', encoding = 'utf-8') as fp:
+                config = json.load(fp)
+        except:
+            pass
+        if not config:
+            return False
+        if isinstance(config, dict):
+            for k in config:
+                v = config[k]
+                if k in self.config:
+                    self.config[k] = v
+        return True
+
+    def save (self):
+        with codecs.open(self.cfgname, 'w', encoding = 'utf-8') as fp:
+            json.dump(self.config, fp)
+        return True
+
+    # set terminal color
+    def console (self, color):
+        if sys.platform[:3] == 'win':
+            import ctypes
+            kernel32 = ctypes.windll.LoadLibrary('kernel32.dll')
+            GetStdHandle = kernel32.GetStdHandle
+            SetConsoleTextAttribute = kernel32.SetConsoleTextAttribute
+            GetStdHandle.argtypes = [ ctypes.c_uint32 ]
+            GetStdHandle.restype = ctypes.c_size_t
+            SetConsoleTextAttribute.argtypes = [ ctypes.c_size_t, ctypes.c_uint16 ]
+            SetConsoleTextAttribute.restype = ctypes.c_long
+            handle = GetStdHandle(0xfffffff5)
+            if color < 0: color = 7
+            result = 0
+            if (color & 1): result |= 4
+            if (color & 2): result |= 2
+            if (color & 4): result |= 1
+            if (color & 8): result |= 8
+            if (color & 16): result |= 64
+            if (color & 32): result |= 32
+            if (color & 64): result |= 16
+            if (color & 128): result |= 128
+            SetConsoleTextAttribute(handle, result)
+        else:
+            if color >= 0:
+                foreground = color & 7
+                background = (color >> 4) & 7
+                bold = color & 8
+                sys.stdout.write(" \033[%s3%d;4%dm"%(bold and "01;" or "", foreground, background))
+                sys.stdout.flush()
+            else:
+                sys.stdout.write(" \033[0m")
+                sys.stdout.flush()
+        return 0
+
+    # new record
+    def update (self, kana, elapse):
+        if kana not in self.config:
+            return False
+        if elapse is None:
+            return False
+        self.config[kana].append(elapse)
+        if len(self.config[kana]) > self.limit:
+            self.config[kana] = self.config[kana][-self.limit:]
+        return True
+    
+    # average score
+    def average (self, kana):
+        records = self.config.get(kana)
+        if not records:
+            return None
+        return float(sum(records)) / len(records)
+
+    # best score
+    def best (self, kana):
+        records = self.config.get(kana)
+        if not records:
+            return None
+        return min(records)
+
+
+
+#----------------------------------------------------------------------
+# entry
+#----------------------------------------------------------------------
+if __name__ == '__main__':
+    def test1():
+        cfg = configure()
+        # cfg.config['a'] = [1,2,3]
+        cfg.save()
+        print(cfg.config)
+        return 0
+    test1()
+
 
 
